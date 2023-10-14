4f5045da9500ab0970f8c71709b44d8c78a84e55
tommy <bigbosslunch@yandex.ru>
2023-10-14 20:50:30 +0300

    Initial commit

--- src/test/java/com/test/TaskControllerTest.java
+++ src/test/java/com/test/TaskControllerTest.java
@@ -4,8 +4,10 @@ import com.project.Application;
 import com.project.controller.TaskController;
 import com.project.helpers.StatusEnum;
 import com.project.model.Task;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,6 +25,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 @SpringBootTest(classes = Application.class)
 @TestPropertySource("/test.properties")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @AutoConfigureMockMvc
 public class TaskControllerTest {
 
